7a19ce78ba996c4a04e675dbe3c23896daa6c8ea
ParsaBordbar <parsab71@gmail.com>
2024-12-17 16:28:14 +0330

    refactor: express.gp package add code to files, app, index annd routes

--- express/express.go
+++ express/express.go
@@ -1,84 +1,142 @@
 package express
 
 import (
-	// "fmt"
-    "os"
-    "log"
-    "os/exec"
+	"log"
+	"os"
+	"os/exec"
 )
 
-// To implement: enums and arguments --flags for pure & ts
-func Express(proj_name string) {
-    
-    // type flag string
-    // const (
-    //     pure    Flag = "pure"
-    //     ts      Flag = "ts"
-    // )
+// Express initializes a basic Express.js project structure
+func Express(projName string) {
+	outputDir := "output"
 
-    // To implement: change output to desired route in next update
-    outputDir := "output"
+	// Create or cd to output dir
+	createOrNavigate(outputDir)
 
-    if _, err := os.Stat(outputDir); os.IsNotExist(err) {
-        if err := os.Mkdir(outputDir, 0755); err != nil {
-            log.Fatalf("Failed to create project directory: %v", err)
-        }
-    }
-    if err := os.Chdir(outputDir); err != nil {
-        log.Fatalf("Failed to change directory to %s: %v", outputDir, err)
-    }
-    
-    // Create project directory if not exists
-    if _, err := os.Stat(proj_name); os.IsNotExist(err) {
-        if err := os.Mkdir(proj_name, 0755); err != nil {
-            log.Fatalf("Failed to create project directory: %v", err)
-        }
-    }
-    if err := os.Chdir(proj_name); err != nil {
-        log.Fatalf("Failed to change directory to %s: %v", proj_name, err)
-    }
+	// Create or cd to project dir
+	createOrNavigate(projName)
 
-    // npm package init
-    terminal := exec.Command("npm", "init", "-y") 
-    terminal.Stdout = os.Stdout
-    terminal.Run()
+	// Install NPM packages
+	runCommand("npm", "init", "-y")
+	runCommand("npm", "install", "express", "nodemon", "joi", "mongoose", "helmet", "morgan", "axios")
 
-    terminal2 := exec.Command("npm", "install", "express", "nodemon", "joi", "mongoose", "helmet", "morgan", "axios")
-    terminal2.Stdout = os.Stdout
-    terminal2.Stderr = os.Stderr
-    err := terminal2.Run()
-    if err != nil {
-        log.Printf("Error installing packages: %v\n", err)
+	// Create subdir and files
+	subDirs := []string{"routes", "controllers", "middlewares", "models", "configs", "services"}
+	createDirectories(subDirs)
+
+	fileNames := []string{"index.js", "app.js"}
+	createFiles(fileNames)
+
+    serverJSCode := `
+const app = require('./app');
+const config = require('./configs/config')
+const mongoose = require('mongoose');
+
+mongoose.connect(config.mongoose.url)
+    .then(() => console.log("Connected to Mongodb :)"))
+    .catch((err) => console.error("Could not connect to Mongodb.", err));
+
+const port = config.port;
+app.listen(port, () => console.log("Listening on Port"));
+`
+writeFile("index.js", serverJSCode)
+
+    appJsCode := `
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const routes = require('./routes/routes')
+
+const app = express();
+
+app.use(cors());
+app.options('*', cors());
+app.use(morgan('combined'));
+app.use(helmet());
+
+app.use('/api/v1', routes);
+
+module.exports = app;
+    `
+    writeFile("app.js", appJsCode)
+
+    routesJsCode := `
+const express = require('express');
+const router = express.Router();
+
+const routes = [
+    {
+        path: '/path',
+        route: route
     }
+];
 
-    // Create Sub-dir and files if not exist
-    dir_names := [5] string {"routes", "controllers", "middlewares", "models", "configs"}
-	file_names := [2] string {"index.js", "app.js"}
+routes.forEach((route) => {
+    router.use(route.path, route.route);
+});
 
-    // Create sub-dir
-    for _, dir := range dir_names {
-        if _, err := os.Stat(dir); os.IsNotExist(err) {
-            if err := os.Mkdir(dir, 0755); err != nil {
-                log.Printf("Failed to create directory %s: %v", dir, err)
-            }
-        }
-    }
-    // Create Files
-	for _, file := range file_names {
+module.exports = router;
+    `
+	// Create `routes.js` in the routes directory if not exist
+	if err := os.Chdir("routes"); err != nil {
+		log.Fatalf("Failed to change directory to routes: %v", err)
+	}
+	if _, err := os.Stat("routes.js"); os.IsNotExist(err) {
+		writeFile("routes.js", routesJsCode)
+	}
+
+	log.Println("Express.js project setup completed successfully.")
+}
+
+func createOrNavigate(dir string) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err := os.Mkdir(dir, 0755); err != nil {
+			log.Fatalf("Failed to create directory %s: %v", dir, err)
+		}
+	}
+	if err := os.Chdir(dir); err != nil {
+		log.Fatalf("Failed to change directory to %s: %v", dir, err)
+	}
+}
+
+func runCommand(name string, args ...string) {
+	cmd := exec.Command(name, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Failed to execute command %s: %v", name, err)
+	}
+}
+
+func createDirectories(dirs []string) {
+	for _, dir := range dirs {
+		if _, err := os.Stat(dir); os.IsNotExist(err) {
+			if err := os.Mkdir(dir, 0755); err != nil {
+				log.Printf("Failed to create directory %s: %v", dir, err)
+			}
+		}
+	}
+}
+
+func createFiles(files []string) {
+	for _, file := range files {
 		if _, err := os.Stat(file); os.IsNotExist(err) {
 			if _, err := os.Create(file); err != nil {
 				log.Printf("Failed to create file: %v", err)
 			}
 		}
 	}
+}
 
-    // Go to dir routes and create routes.js if not exist
-    if err := os.Chdir("routes"); err != nil {
-        log.Fatalf("Failed to change directory to routes: %v", err)
-    }
-    if _, err := os.Stat("routes.js"); os.IsNotExist(err) {
-        if _, err := os.Create("routes.js"); err != nil {
-            log.Printf("Failed to create file routes.js: %v", err)
-        }
-    }
+func writeFile(fileName string, content string) {
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatalf("Failed to open file %s: %v", fileName, err)
+	}
+	defer file.Close()
+
+	if _, err := file.WriteString(content); err != nil {
+		log.Fatalf("Failed to write to file %s: %v", fileName, err)
+	}
 }
